cbebe4d56b7776da093231723617ac97d0ccabc3
Fernando Lepore <juliofernandolepore@gmail.com>
2023-02-19 02:31:32 -0300

    probando css dentro de styled-components

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -1,11 +1,9 @@
+const Eventos = () => {
+  return (
+    <>
+      <h1 className="h-20 bg-black text-white">EVENTOS</h1>
+    </>
+  );
+};
 
-const Eventos = ()=>{
-    return (
-        <>
-        <h1 className="h-20 bg-black text-white">EVENTOS</h1>
-        </>
-    
-    )
-}
-
-export default Eventos
+export default Eventos;
--- src/pages/Multimedia.jsx
+++ src/pages/Multimedia.jsx
@@ -1,15 +1,28 @@
-import ParrafoEstilizado from "../styledComponents/Parrafo"
+import styled from "styled-components";
 
-const Multimedia = ()=>{
-    return (
-        <>
-        <ParrafoEstilizado>
-        <p>multimedia</p>
-        </ParrafoEstilizado>
-        
-        </>
-    
-    )
-}
+const Multimedia = () => {
+/* herramienta muy util para escribir css nativo dentro de jsx sin necesidad de archivos de estilos
+modificando esclusivamente el componente donde estoy parado */
 
-export default Multimedia
+  const Titulo = styled.h1`
+    color: green;
+    font-size: 60px;
+    background-color: black;
+    padding: 30px;
+  `;
+  const Parrafo = styled.p`
+    color: yellow;
+    font-size: 40px;
+    background-color: black;
+    border: 4px solid white;
+    padding: 10px;
+  `;
+  return (
+    <>
+      <Titulo>algo para practicar styled components</Titulo>
+      <Parrafo>trabajando con parrafos</Parrafo>
+    </>
+  );
+};
+
+export default Multimedia;
