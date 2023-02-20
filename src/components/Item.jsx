4fdb382ffacfb35a3902c685d6c02e4833796b22
Fernando Lepore <juliofernandolepore@gmail.com>
2023-02-20 19:02:58 -0300

    Link y useParams

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const Item = ({ i }) => {
   /* COMPONENTE MAQUETADOR UNICAMENTE */
   /* componente para el maquetador o render de los elementos donde se alojaran por
@@ -7,8 +8,8 @@ const Item = ({ i }) => {
 /* el atributo permalink traido en el arreglo devuelto en la api permite redireccion al producto en cuestion */    
   return (
     <>
-      <div className="m-3 p-5 border border-5">
-        <a href={i.permalink} target={"_blank"} className="text-start " rel={"noreferrer"}>
+    <Link to={"/item/" + i.permalink}>
+      <div className="m-3 p-5 border border-5 text-start">
           <img src={i.thumbnail} className="card-img-top" alt={i.title} />
           <div className="card-body">
             <h5 className="card-title">{i.title}</h5>
@@ -16,8 +17,8 @@ const Item = ({ i }) => {
               <b>${i.price}</b>
             </p>
           </div>
-        </a>
       </div>
+      </Link>
     </>
   );
 };
