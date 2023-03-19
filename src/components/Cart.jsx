b47ac0b34d4f0e468660bae0999a7d3ab561f1e5
Fernando Lepore <juliofernandolepore@gmail.com>
2023-03-19 19:36:47 -0300

    styled component agregando estilo a componente cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,11 +2,41 @@ import { useContext } from "react";
 import {CartContext} from "../context/CartContext";
 import basura from "../images/basura.svg";
 import {Link} from "react-router-dom";
-//import swal from "sweetalert"
+import styled from "styled-components";
 
 const Cart = () => {
-  //const alerta = swal("Ups!", "No existen productos en tu carrito!", "warning");     
+       
   const {cart, removeItem, limpiar, cartTotal, cartSum} = useContext(CartContext); 
+  const SeleccionTitulo = styled.h1 `
+    color: black;
+    background-color:green;
+    border-radius: 0.2rem;
+    padding: 1rem;
+    margin: 1rem;
+  ` ;
+  const CarritoVacio = styled.h3 `
+    color: black;
+    background-color: red;
+    font-size: 2rem;
+    border-radius: 0.5rem;
+    padding: 1.5rem;
+    margin: 3rem;    
+  ` ;
+  const ConfirmarProceso = styled.button `
+    color: white;
+    background-color: green;
+    border-radius: 0.3rem;
+    margin: 0.5rem;
+    padding: 0.3rem;
+  `;
+
+  const BotonVaciar = styled.button `
+    color: white;
+    background-color: red;
+    border-radius: 0.3rem;
+    margin-right: 2rem;
+    padding: 1rem;
+  `
 
   if(cartTotal() === 0){
     
@@ -15,7 +45,7 @@ const Cart = () => {
     <div className="container">
                 <div className="row">
                     <div className="col-md-12">
-                    <div className="alert alert-warning text-center" role="alert">el carrito no contiene ningun producto!</div>
+                    <CarritoVacio>El carrito de compras esta vacio!</CarritoVacio>
                 </div>
             </div>
     </div>)    
@@ -24,11 +54,11 @@ const Cart = () => {
   return (    
     <div className="container my-5">
     <div className="row">
-        <h1 className="text-center">Productos Seleccionados</h1>
+        <SeleccionTitulo>Productos Seleccionados</SeleccionTitulo>
         <div className="col-md-12">
             <table className="table">
                 <tr>
-                    <td className="text-end" colSpan={5}><Link className="btn btn-warning bg-warning" onClick={() => {limpiar()}}>Vaciar Carrito</Link></td>
+                    <td className="text-end" colSpan={5}><BotonVaciar><Link onClick={() => {limpiar()}}>Vaciar Carrito</Link></BotonVaciar></td>
                 </tr>
                 {
                     cart.map(item => ( 
@@ -45,7 +75,7 @@ const Cart = () => {
                     <td colSpan={2}>&nbsp;</td>
                     <td className="text-center">Total a Pagar</td>
                     <td className="text-center"><b>${cartSum()}</b></td>
-                    <td className="text-end"><Link to={"/checkout"} className="btn btn-warning bg-warning">Finalizar el proceso de Compra</Link></td>
+                    <td className="text-end"><Link to={"/checkout"}> <ConfirmarProceso> confirmar seleccion y continuar proceso </ConfirmarProceso> </Link> </td>
                 </tr>
             </table>
         </div>
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <div className="container">
             <div className="row my-5">
                 <div className="col text-center">
-                <p className="justify-content-center"><img src={error404} alt="Sony Argentina" width={"80"} /></p>
+                <p className="justify-content-center"><img src={error404} alt="pagina no encontrada" width={"80"} /></p>
                 <h1>Error 404!</h1>                    
                 <h3>Pagina no encontrada!</h3>
                 </div>
