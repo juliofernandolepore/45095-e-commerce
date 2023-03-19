6e4342ae59735b0fee51a163a0db49db173f20cd
Fernando Lepore <juliofernandolepore@gmail.com>
2023-03-19 16:09:38 -0300

    trabajando styled components sobre cartWidget

--- src/components/CardWidget.jsx
+++ src/components/CardWidget.jsx
@@ -1,18 +1,33 @@
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 import cart from "../images/cart.svg";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 
 const CardWidget = () => {
+  const LinkCart = styled.div `
+    background-color: green;
+    padding: 0.7rem;
+    border-radius: 0.7rem;
+    height: 50px;
+    width: 50px;
+    position: absolute;
+    top: 0;
+    left: 15px;
+  `;
+  const SpanCart = styled.span `
+    color: white;
+    padding: 0.7rem;
+    margin: 0.7rem;    
+  `;
   const {cartTotal} = useContext(CartContext);
 
   return (
     
-    (cartTotal() > 0) ? <Link to={"/cart"} className="bg-green-400 w-14 m-1 p-1 rounded text-center">        
-        <img src={cart} alt={"mi carrito"} width={24} />
-        <span className="position-absolute top-0 start-100 traslate-middle badge rounded-pill bg-danger">
-          {cartTotal()}</span>
-       </Link> : ""
+    (cartTotal() > 0) ? <LinkCart><Link to={"/cart"}>        
+        <img src={cart} alt={"carrito"} width={18} />
+        <SpanCart>{cartTotal()}</SpanCart>
+       </Link></LinkCart> : ""
        )  
 }
 
