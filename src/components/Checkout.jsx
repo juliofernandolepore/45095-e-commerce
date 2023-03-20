46c748515b338447ade992b11e52c7023e2b1320
Fernando Lepore <juliofernandolepore@gmail.com>
2023-03-19 21:15:45 -0300

    arreglando estilos

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,6 +3,7 @@ import { useContext, useState } from "react"
 import { CartContext} from "../context/CartContext"
 import { collection, addDoc } from "firebase/firestore";
 import { Navigate } from "react-router";
+import styled from "styled-components";
 
 const Checkout = () => { 
     
@@ -37,12 +38,36 @@ const Checkout = () => {
             limpiar();
         });
     }
+        const FinalizarCompraH1 = styled.h1 `
+        color: white;
+        background-color: red;
+        padding: 1rem;
+        margin-left: 1rem;
+        margin-top: 1rem;
+        font-weight: 700;
+        `
+        const ImagenCompra = styled.img `
+            margin: 2rem;
+            border-radius: 0.2rem;
+            border: solid red 2px;
+        `
+        const TituloJuego = styled.td `
+          color: red;
+          font-size: 2rem;
+          font-weight: 600;
+          padding-left: 2rem;  
+        `;
+        const TotalPagar = styled.b `
+            color: red;
+            font-size: 1rem;
+            font-weight: 800;
+        `
 
     return (
         <div className="container my-5">
             <div className="row">
-                <h1 className="text-center">Finalizar Compra</h1>
-                <div className="col-md-3">
+                <FinalizarCompraH1>Finalizar Compra</FinalizarCompraH1>
+                <div>
                     <form>
                         <div class="mb-3">
                             <label htmlFor="nombre" class="form-label">Nombre</label>
@@ -56,9 +81,8 @@ const Checkout = () => {
                             <label htmlFor="telefono" class="form-label">Teléfono</label>
                             <input type="text" class="form-control" id="telefono" onInput={(e) => {setTelefono(e.target.value)}} />
                         </div>
-                        <button type="button" class="btn btn-warning" onClick={generarOrden}>Generar Orden</button>
-                    </form>
-                
+                        <button type="button" class="btn btn-warning" onClick={generarOrden}>Comprar y Generar Orden</button>
+                    </form>                
             </div>
             <div className="col-md-9">
                 <table className="table">
@@ -66,8 +90,8 @@ const Checkout = () => {
                     {
                         cart.map(item => ( 
                             <tr key={item.id}>
-                                <td className="text-start" width="10%"><img src={item.imagen} alt={item.nombre} width={120} /></td>
-                                <td className="text-start align-middle" width="40%">{item.nombre}</td>
+                                <td className="text-start" width="10%"><ImagenCompra src={item.imagen} alt={item.nombre} width={120} /></td>
+                                <TituloJuego className="text-start align-middle" width="40%">{item.nombre}</TituloJuego>
                                 <td className="text-center align-middle" width="20%">Unid. <b>{item.quantity}</b> x ${item.precio}</td>
                                 <td className="text-center align-middle" width="20%">${item.quantity * item.precio}</td>
                                 
@@ -77,7 +101,7 @@ const Checkout = () => {
                     <tr>
                         <td colSpan={2}>&nbsp;</td>
                         <td className="text-center">Total a Pagar</td>
-                        <td className="text-center"><b>${cartSum()}</b></td>
+                        <td className="text-center"><TotalPagar>${cartSum()}</TotalPagar></td>
                 
                     </tr>
                 </table>
